51552582294736b9ed14f7e6d90053ec12521780
DerpyMissile <hmai015@ucr.edu>
2022-07-22 11:43:17 -0700

    saving just in case I accidentally create an infinite loop

--- You Are Painter/Assets/Scripts/OverworldMovement.cs
+++ You Are Painter/Assets/Scripts/OverworldMovement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class OverworldMovement : MonoBehaviour
 {
+    [SerializeField] Rigidbody painter;
+    float timeHoldButton = 0.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,13 @@ public class OverworldMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Mathf.Abs(Input.GetAxis("Horizontal")) != 0){
+            painter.velocity = new Vector3(Input.GetAxis("Horizontal") * 5.0f, painter.velocity.y, 0);
+        }
+        if(Mathf.Abs(Input.GetAxis("Vertical")) == 1 && timeHoldButton<1.0f){
+            timeHoldButton = timeHoldButton + Time.deltaTime;
+            painter.velocity = new Vector3(painter.velocity.x, Input.GetAxis("Vertical") * 5.0f, 0);
+        }
+        timeHoldButton = 0;
     }
 }
